62d98c5aab6c489ba0250381da2768048d3b5043
Kevin <kevin.sweet@sendgrid.com>
2014-09-04 01:06:37 -0600

    google plus share counter now works

--- about.md
+++ about.md
@@ -8,7 +8,7 @@ I'm a front end developer living in Boulder, CO. I work for [SendGrid](http://se
 
 I like to travel, rock climb, hike, snowboard, and learn languages.
 
-I spend a lot of time thinking about all of the things that I want to accomplish in life. You can check out [my bucketlist](/my-bucketlist/) to see what I find important.
+I spend a lot of time thinking about all of the things that I want to accomplish in life. You can check out [my bucket list](/my-bucket-list/) to see what I find important.
 
 You can look at all of the code for this website in [its GitHub repository](https://github.com/DynamicDyno/dynamicdyno.github.io). Feel free to fork and do as you please.
 
--- javascript/application.js
+++ javascript/application.js
@@ -15,68 +15,70 @@ initialize_post();
 
 function initialize_post() {
   var share_buttons = $('.share-buttons');
-  var share_buttons_position = share_buttons.offset();
+  if (share_buttons.length) {
+    var share_buttons_position = share_buttons.offset();
 
-  $(window).scroll(function() {
-    if($(this).width() > 979) {
-      if($(this).scrollTop() > ( share_buttons_position.top - 88 ) ) {
-        share_buttons.css('position','fixed').css('top','88px');
-      } else {
-        share_buttons.css('position','absolute').css('top', '0');
+    $(window).scroll(function() {
+      if($(this).width() > 979) {
+        if($(this).scrollTop() > ( share_buttons_position.top - 88 ) ) {
+          share_buttons.css('position','fixed').css('top','88px');
+        } else {
+          share_buttons.css('position','absolute').css('top', '0');
+        }
       }
-    }
-  });
+    });
 
-  $(window).resize(function() {
-    // on window resize, unset any styles to the share buttons
-    if($(this).width() < 979) {
-      $('.share-buttons').attr('style', '');
-    }
-  });
+    $(window).resize(function() {
+      // on window resize, unset any styles to the share buttons
+      if($(this).width() < 979) {
+        $('.share-buttons').attr('style', '');
+      }
+    });
 
-  $('#shareme').sharrre({
-    share: {
-      twitter: true,
-      facebook: true,
-      googlePlus: true
-    },
-    template: '{facebook},{twitter},{total}',
-    enableHover: false,
-    enableTracking: true,
-    render: function(api, options){
-      $(api.element).on('click', '.twitter', function() {
-        api.openPopup('twitter');
-      });
-      $(api.element).on('click', '.facebook', function() {
-        api.openPopup('facebook');
-      });
-    }
-  });
+    $('#shareme').sharrre({
+      share: {
+        twitter: true,
+        facebook: true,
+        googlePlus: true
+      },
+      urlCurl: 'http://become-fluent.com/sharrre.php',
+        template: '{facebook},{twitter},{total}',
+      enableHover: false,
+      enableTracking: true,
+      render: function(api, options){
+        $(api.element).on('click', '.twitter', function() {
+          api.openPopup('twitter');
+        });
+        $(api.element).on('click', '.facebook', function() {
+          api.openPopup('facebook');
+        });
+      }
+    });
 
-  // make share links open in popup
-  $('.share-link').on('click', function(e) {
-    console.log('test');
-    var _this = $(this);
-    popupCenter(_this.attr('href'), _this.find('.text').html(), 580, 470);
-    e.preventDefault();
-  });
+    // make share links open in popup
+    $('.share-link').on('click', function(e) {
+      var _this = $(this);
+      popupCenter(_this.attr('href'), _this.find('.text').html(), 580, 470);
+      e.preventDefault();
+    });
 
-  var popupCenter = function(url, title, w, h) {
-    // Fixes dual-screen position                         Most browsers      Firefox
-    var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-    var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+    var popupCenter = function(url, title, w, h) {
+      // Fixes dual-screen position                         Most browsers      Firefox
+      var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+      var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
 
-    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+      var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+      var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
-    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-    var top = ((height / 3) - (h / 3)) + dualScreenTop;
+      var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+      var top = ((height / 3) - (h / 3)) + dualScreenTop;
 
-    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+      var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 
-    // Puts focus on the newWindow
-    if (window.focus) {
-      newWindow.focus();
-    }
-  };
+      // Puts focus on the newWindow
+      if (window.focus) {
+        newWindow.focus();
+      }
+    };
+  }
 }
