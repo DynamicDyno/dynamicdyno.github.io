586627c060c30636e00584624d795a55f8d759cc
Kevin Sweet <kevindsweet@gmail.com>
2019-01-02 10:01:37 -0700

    Delete viglink.js

--- _includes/javascript/viglink.js
+++
@@ -1,7 +0,0 @@
-var vglnk = { key: 'de8417cbd255d90d00c9efaf19b0cc09' };
-
-(function(d, t) {
-  var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
-  s.src = '//cdn.viglink.com/api/vglnk.js';
-  var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
-  }(document, 'script'));
